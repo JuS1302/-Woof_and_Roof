b1782af13e8174aefe1f7d74db4f185330459a9e
Julie Simon <sim.ju@live.fr>
2024-11-21 23:29:32 +0100

    debut avatar

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
     end
 
-
   devise_for :users
   root to: "offers#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
